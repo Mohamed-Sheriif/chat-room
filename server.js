a0d6883c5d96eb9e94ad7d425b452902aaaf039b
MohamedSherif <mohamedsh2782@gmail.com>
2023-01-30 14:11:02 +0200

    the messages is implemented with real-time

--- server.js
+++ server.js
@@ -12,8 +12,24 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 // Run when client connects
-io.on("connection", (socet) => {
+io.on("connection", (socket) => {
   console.log("New WS Connection...");
+
+  // Appear to the single user who connect
+  socket.emit("message", "Welcome to ChatCord");
+
+  // Broadcast when a user connect (appear to all users except this user)
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    // Appear to all the users including the disconnecting user
+    io.emit("message", "A user has let the chat");
+  });
+
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
 server.listen(PORT, () => {
